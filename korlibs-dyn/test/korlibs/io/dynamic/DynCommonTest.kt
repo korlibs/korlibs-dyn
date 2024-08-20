a699c7f21ff541fc61b35879307eb003d9c6a687
soywiz <soywiz@gmail.com>
2024-08-20 12:48:50 +0200

    Update README with a small explanation:

--- korlibs-dyn/test/korlibs/io/dynamic/DynCommonTest.kt
+++ korlibs-dyn/test/korlibs/io/dynamic/DynCommonTest.kt
@@ -3,27 +3,27 @@ package korlibs.io.dynamic
 import kotlin.test.*
 
 class DynCommonTest {
-    val data1 = mapOf("a" to mapOf("b" to 10)).dyn
+val data1 = mapOf("a" to mapOf("b" to 10)).dyn
 
-    @Test
-    fun test() {
-        assertEquals(10, data1["a"]["b"].int)
-    }
+@Test
+fun test() {
+    assertEquals(10, data1["a"]["b"].int)
+}
 
-    @Test
-    fun testBinop() {
-        assertEquals(11, (10.0.dyn + 1.dyn).int)
-        assertEquals(9, (10.0.dyn - 1.dyn).int)
-        assertEquals(20, (10.0.dyn * 2.dyn).int)
-        assertEquals(5, (10.0.dyn / 2.0.dyn).int)
-        assertEquals(0, (10.0.dyn % 2.0.dyn).int)
-        assertEquals(100, (10.0.dyn pow 2.0.dyn).int)
-        assertEquals(2, (3.dyn bitAnd 6.dyn).int)
-        assertEquals(7, (3.dyn bitOr 6.dyn).int)
-        assertEquals(5, (3.dyn bitXor 6.dyn).int)
-        assertEquals(true, (3.dyn and 6.dyn))
-        assertEquals(true, (3.dyn or 6.dyn))
-    }
+@Test
+fun testBinop() {
+    assertEquals(11, (10.0.dyn + 1.dyn).int)
+    assertEquals(9, (10.0.dyn - 1.dyn).int)
+    assertEquals(20, (10.0.dyn * 2.dyn).int)
+    assertEquals(5, (10.0.dyn / 2.0.dyn).int)
+    assertEquals(0, (10.0.dyn % 2.0.dyn).int)
+    assertEquals(100, (10.0.dyn pow 2.0.dyn).int)
+    assertEquals(2, (3.dyn bitAnd 6.dyn).int)
+    assertEquals(7, (3.dyn bitOr 6.dyn).int)
+    assertEquals(5, (3.dyn bitXor 6.dyn).int)
+    assertEquals(true, (3.dyn and 6.dyn))
+    assertEquals(true, (3.dyn or 6.dyn))
+}
 
     @Test
     fun testSEq() {
